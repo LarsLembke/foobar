3cc9629fd0f5966859eb2375f63113c947f93c9d
larslembke <lars46i6@stud.kea.dk>
2021-11-28 17:59:39 +0100

    ghgtygtuy

--- src/components/History.js
+++ src/components/History.js
@@ -3,7 +3,16 @@
 import Order from "./Order";
 
 
-export default function History(){
+export default function History(props){
+
+    if(!props.order) {
+        return null
+    }
+
+    console.log(props.order);
+
+    //get ids, get stored ids
+
     return <div id="history">
         <Order/>
     </div>
